be28db8a2ced4bc37f72c37afdbdb9d2460791c5
Brandon Bae <bkb36@duke.edu>
2023-02-17 00:02:18 -0500

    jar compiled for tcp echo server experiment

--- src/test/java/tcp/TcpTests.java
+++ src/test/java/tcp/TcpTests.java
@@ -5,6 +5,8 @@ import org.junit.*;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
 public class TcpTests {
     private EchoClient client;
 
@@ -15,10 +17,31 @@ public class TcpTests {
     public void setup() {
         client = new EchoClient();
         try {
-            client.startConnection("127.0.0.1", 4444);
+            client.startConnection("127.0.0.1", 6666);
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void givenClient_whenServerEchosMessage_thenCorrect() {
+        String resp1 = null;
+        String resp2 = null;
+        String resp3 = null;
+        String resp4 = null;
+        try {
+            resp1 = client.sendMessage("hello");
+            resp2 = client.sendMessage("world");
+            resp3 = client.sendMessage("!");
+            resp4 = client.sendMessage(".");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("hello", resp1);
+        assertEquals("world", resp2);
+        assertEquals("!", resp3);
+        assertEquals("good bye", resp4);
     }
 
     @After
